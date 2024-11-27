f8447a489b70756d05826e411016b5f5e1d48a3b
Marc_Joeseph_Ruiz <marc.ruiz@my.marianas.edu>
2024-11-27 15:44:50 +1000

    Changes

---
+++ clock.js
@@ -0,0 +1,27 @@
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setClockFace() {
+  // Now the const variable is not global; it's local to the method
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  var time = hours + ":" + minutes + ":" + seconds;
+  const digitalClockFace = document.querySelector('.time');
+  digitalClockFace.innerHTML =time;
+}
+
+// 1000 milliseconds is one second
+setInterval(setClockFace, 1000);
